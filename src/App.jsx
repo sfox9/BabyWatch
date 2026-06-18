447199af118349bcfc3e6e78505b5eb9c6e508ea
sfox9 <sarah.sheats@gmail.com>
2026-06-18 10:39:59 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import SettingsPanel from "./components/SettingsPanel";
 import NotificationsBell from "./components/NotificationsBell";
 import BabyLogo from "./components/Logo";
 import { Btn, LeafMark } from "./components/UI";
+import ChatPanel from "./components/ChatPanel";
+import { isCloudMode } from "./lib/supabaseClient";
 
 const today = new Date();
 
@@ -324,6 +326,15 @@ export default function App() {
       <AddShiftModal
         open={Boolean(addModalKey)} onClose={() => setAddModalKey(null)}
         selectedDate={addModalKey} onAdd={handleAddShift} childrenList={children}
+      />
+      <ChatPanel
+        user={user}
+        members={members}
+        familyId={activeFamilyId || user.familyId}
+        isCloud={isCloudMode()}
+        onFetchMessages={(thread) => store.fetchMessages(user, activeFamilyId || user.familyId, thread)}
+        onSendMessage={(thread, body) => store.sendMessage(user, activeFamilyId || user.familyId, thread, body)}
+        onSubscribe={(cb) => store.subscribeToMessages(user, cb)}
       />
     </div>
   );
